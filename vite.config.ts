acedbec9cb2162a5e26bf5ec361b445a4ba18ff8
srichandankota <kotasc18@gmail.com>
2025-04-07 17:51:41 -0500

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
+// nuxt.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        // Target an environment that supports top-level await
-        target: 'es2022'
-        // Alternatively, you can use 'node18' if you prefer
-    }
+    esbuild: {
+        target: 'esnext'
+    },
+    server: {}
 })
