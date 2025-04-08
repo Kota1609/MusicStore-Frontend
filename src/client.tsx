abec4ab2c436a93226bbd0eec16e537b3b188663
srichandankota <kotasc18@gmail.com>
2025-04-08 13:59:43 -0500

    Update client.tsx

--- src/client.tsx
+++ src/client.tsx
@@ -1,8 +1,16 @@
 import { hydrateRoot } from 'react-dom/client'
 import { StartClient } from '@tanstack/react-start'
-
 import { createRouter } from './router'
 
 const router = createRouter()
 
-hydrateRoot(document, <StartClient router={router} />)
+// Export the app component for SSR
+const App = () => <StartClient router={router as any} />
+
+// Default export for SSR
+export default App
+
+// Client-side hydration
+if (typeof window !== 'undefined') {
+  hydrateRoot(document, <App />)
+}
